60ebc2d0c526428b93abb68b43aae424cd7f8e95
Ezequiel <e.troncoso@globaltank.eu>
2023-05-27 18:34:28 +0200

    changes in app and created flask

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '¡Hola, Flask!'
+
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    audio_file = request.files['audio']
+    # Obtén el nombre original del archivo
+    filename = audio_file.filename
+    # Obtén la ruta absoluta de la carpeta "files"
+    folder_path = os.path.join(os.path.dirname(__file__), 'files')
+    # Combina la ruta de la carpeta "files" con el nombre de archivo
+    save_path = os.path.join(folder_path, filename)
+    import pdb;pdb.set_trace()
+    # Guarda el archivo en la ruta especificada
+    audio_file.save(save_path)
+
+
+
+    return 'Archivo de audio recibido correctamente'
