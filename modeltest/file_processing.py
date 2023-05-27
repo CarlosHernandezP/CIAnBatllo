1ed988519435e9929c9d42529a493e3691ac5c27
LuchoGabba <lgabbanelli@hotmail.com>
2023-05-27 21:33:04 +0200

    Update tts

--- modeltest/file_processing.py
+++ modeltest/file_processing.py
@@ -1,5 +1,5 @@
 import whisper
-import ggts import gTTS
+from gtts import gTTS
 
 
 def stt(audio_path : str) -> str:
@@ -11,4 +11,9 @@ def stt(audio_path : str) -> str:
 
     return input_string
 
-def tts(text_to_convert : str=None) ->:
+def tts(text_to_convert : str=None, save_path : str=None, lang : str='it') ->None:
+    tts = gTTS(text=text_to_convert, lang=lang)
+
+    tts.save(save_path)  # Save the TTS result to an audio file
+
+    return 
