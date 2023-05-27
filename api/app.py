5acb8efae780a8997bf57c19f19a8daad7295ec4
Ezequiel <e.troncoso@globaltank.eu>
2023-05-27 22:03:16 +0200

    create view interface

--- api/app.py
+++ api/app.py
@@ -1,32 +1,36 @@
-from flask import Flask, request
-# import transcribe as transcribe from a directory that is ../modeltest
+from flask import Flask, request, render_template
 import sys
-sys.path.append('../')
-import modeltest.transcribe as transcribe
 import os
+
+# Asegúrate de que el directorio "../modeltest" esté en el PYTHONPATH para poder importar el módulo correctamente
+sys.path.append('../modeltest')
+import transcribe as transcribe
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '¡Hola, Flask!'
+    return render_template('index.html')
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
     audio_file = request.files['audio']
+    # Obtén el valor del campo 'language' del formulario
+    lang = request.form['lang']
     # Obtén el nombre original del archivo
     filename = audio_file.filename
     # Obtén la ruta absoluta de la carpeta "files"
-    
     folder_path = os.path.join(os.path.dirname(__file__), 'files')
-    # make sure that the folder exists
+    # Asegúrate de que la carpeta exista
     os.makedirs(folder_path, exist_ok=True)
-
     # Combina la ruta de la carpeta "files" con el nombre de archivo
     save_path = os.path.join(folder_path, filename)
     # Guarda el archivo en la ruta especificada
     audio_file.save(save_path)
+
     result = transcribe.transcribe(save_path)
 
+    return render_template('result.html', result=result,lang=lang)
 
-    return result #'Archivo de audio recibido correctamente'
+if __name__ == '__main__':
+    app.run()
