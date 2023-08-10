cef47fabba5ba6483a58aa80ea6bd1de82fd73fe
CarlosHernandezP <crherperez95@gmail.com>
2023-08-10 22:01:56 +0000

    "update code with api functionality"

--- image_generation/text_to_image.py
+++ image_generation/text_to_image.py
@@ -11,9 +11,17 @@ from torchvision import utils
 from torchvision.transforms import functional as TF
 from tqdm.notebook import trange, tqdm
 
-sys.path.append('/v-diffusion-pytorch')
+import os
 
-import ipdb; ipdb.set_trace()
+# Get the path of the current script
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the directory containing my_module.py
+module_path = os.path.join(current_path, 'v_diffusion_pytorch')
+
+# Add the directory to sys.path
+sys.path.append(module_path)
+# Add the module path to the Py
 from diffusion import get_model
 
 import argparse
@@ -42,25 +50,34 @@ def callback(info):
         nrow = math.ceil(info['denoised'].shape[0] ** 0.5)
         grid = utils.make_grid(info['denoised'], nrow, padding=0)
         tqdm.write(f'Step {info["i"]} of 50 ,lie, sigma {info["sigma"]:g}:')
-        display.display(K.utils.to_pil_image(grid))
+        K.utils.to_pil_image(grid).save(f"progress_step_{info['i']}.png")
+        #display.display(K.utils.to_pil_image(grid))
         tqdm.write(f'')
 
-def run( weight: int = 3, steps: int = 50,
-        seed: int = 0,  n_images: int = 4):
+def generate_image(prompt: str, steps: int = 50, weight: int = 3, seed: int = 0):
+
+    inner_model = get_model('cc12m_1_cfg')()
+    _, side_y, side_x = inner_model.shape
+    inner_model.load_state_dict(torch.load('v_diffusion_pytorch/checkpoints/cc12m_1_cfg.pth', map_location='cpu'))
+    inner_model = inner_model.half().cuda().eval().requires_grad_(False)
+    model = K.external.VDenoiser(inner_model)
+    clip_model = clip.load(inner_model.clip_model, jit=False, device='cpu')[0]
+
+    prompt = args.prompt
+    target_embed = clip_model.encode_text(clip.tokenize(prompt)).float().cuda()
 
     gc.collect()
     torch.cuda.empty_cache()
     torch.manual_seed(seed)
     sigmas = K.sampling.get_sigmas_karras(steps, 1e-2, 160, device='cuda')
-    x = torch.randn([n_images, 3, side_y, side_x], device='cuda') * sigmas[0]
+    x = torch.randn([4, 3, side_y, side_x], device='cuda') * sigmas[0]
     model_wrap = CFGDenoiser(torch.cuda.amp.autocast()(model), weight)
-    extra_args = {'clip_embed': target_embed.repeat([n_images, 1])}
+    extra_args = {'clip_embed': target_embed.repeat([4, 1])}
     outs = K.sampling.sample_lms(model_wrap, x, sigmas, extra_args=extra_args, callback=callback)
     tqdm.write('Done!')
     for i, out in enumerate(outs):
         filename = f'out_{i}.png'
         K.utils.to_pil_image(out).save(filename)
-        display.display(display.Image(filename))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -75,19 +92,10 @@ if __name__ == '__main__':
     parser.add_argument('--n_images', type=int, default=4, help='Number of images to generate')
     args = parser.parse_args()
 
+    generate_image(args.prompt, args.steps, args.weight, args.seed)
 
 
-    inner_model = get_model('cc12m_1_cfg')()
-    _, side_y, side_x = inner_model.shape
-    inner_model.load_state_dict(torch.load('v-diffusion-pytorch/checkpoints/cc12m_1_cfg.pth', map_location='cpu'))
-    inner_model = inner_model.half().cuda().eval().requires_grad_(False)
-    model = K.external.VDenoiser(inner_model)
-    clip_model = clip.load(inner_model.clip_model, jit=False, device='cpu')[0]
 
-    prompt = args.prompt
-    target_embed = clip_model.encode_text(clip.tokenize(prompt)).float().cuda()
-
-    run(args.weight, args.steps, args.seed, args.n_images)
         
 
 
