eccb40511cccaa445b93a2be8492158f94d02c14
CarlosHernandezP <crherperez95@gmail.com>
2023-12-17 20:56:15 +0100

    Make jumoing game work and add hand gesture control

--- camera/camera_record.py
+++ camera/camera_record.py
@@ -1,3 +1,4 @@
+# camera_record.py
 import cv2
 import mediapipe as mp
 
@@ -6,45 +7,41 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 mp_drawing = mp.solutions.drawing_utils
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
+def update_hand_gesture(queue):
+    # Initialize the webcam
+    cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-        print("Ignoring empty camera frame.")
-        continue
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            continue
 
-    # Convert the color from BGR to RGB and process it
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
+        # Convert the color from BGR to RGB for MediaPipe processing
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(rgb_image)
 
-    # Draw the pose annotations
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.pose_landmarks:
-        mp_drawing.draw_landmarks(
-            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        # Convert back to BGR for displaying
+        bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
 
-        # Get landmarks for left and right wrists
-        landmarks = results.pose_landmarks.landmark
-        right_wrist = landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
-        left_wrist = landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
-        right_shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+        if results.pose_landmarks:
+            # Draw landmarks for debugging
+            mp_drawing.draw_landmarks(
+                bgr_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        # Check if hands are raised
-        if right_wrist.y < right_shoulder.y:
-            image[:, :, 0] = image[:, :, 0] * 0.5  # Reduce red and green channels
-            image[:, :, 1] = image[:, :, 1] * 0.5
+            # Get landmarks for left wrist and shoulder
+            landmarks = results.pose_landmarks.landmark
+            left_wrist = landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+            left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
 
-        if left_wrist.y < left_shoulder.y:
-            image[:, :, 1] = image[:, :, 1] * 0.5  # Reduce green and blue channels
-            image[:, :, 2] = image[:, :, 2] * 0.5
+            # Check if left hand is raised
+            is_raised = left_wrist.y < left_shoulder.y
+            queue.put(is_raised)
 
-    # Display the image
-    cv2.imshow('MediaPipe Pose with Color Change', image)
+           
 
-    if cv2.waitKey(5) & 0xFF == 27:
-        break
+        # Display the image with landmarks for debugging
+        cv2.imshow('MediaPipe Pose', bgr_image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
-cap.release()
+    cap.release()
